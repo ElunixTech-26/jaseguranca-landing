024564ac7ac3a4c8a10d0509e0451fd313684571
Lucas Lopes <lp.skt25@gmail.com>
2026-03-06 01:08:14 -0300

    fix: ajustes no hero

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -39,28 +39,24 @@ export default function Hero() {
         </h1>
         
         <p className="text-(--color-text-secondary) text-lg md:text-xl max-w-2xl mb-10">
-            {/* Sistemas inteligentes de segurança e automação residencial e empresarial. */}
-            {/* Segurança e tecnologia <br/> sob seu controle. */}
-            soluções de segurança eletrônica de alta qualidade <br/>para proteger seus negócios, residências e propriedades. 
+            Soluções de segurança eletrônica de alta qualidade <br/>para proteger seus negócios, residências e propriedades. 
         </p>
 
-      <div className="flex flex-col md:flex-row gap-4">
-        {/* Link para o formulário (âncora na mesma página) */}
-        <Link 
-          href="/#contato" 
-          className="primary-button text-white !border-none text-center"
-        >
-          Solicitar Orçamento
-        </Link>
+        <div className="flex flex-col md:flex-row gap-4">
+          <Link 
+            href="/#contato" 
+            className="primary-button text-white !border-none text-center"
+          >
+            Solicitar Orçamento
+          </Link>
 
-        {/* Link para a seção de serviços na Home */}
-        <Link 
-          href="/#services" 
-          className="px-8 py-3 border border-white text-white rounded-full hover:bg-white hover:text-black transition text-center"
-        >
-          Nossos Serviços
-        </Link>
-      </div>
+          <Link 
+            href="/#services" 
+            className={styles.secondaryButton}
+          >
+            Nossos Serviços
+          </Link>
+        </div>
       </div>
     </section>
   );
